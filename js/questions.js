3ee8ee74f08265d0ffece5a6317283ce58ea4bac
MarAcres <114613161+MarAcres@users.noreply.github.com>
2023-02-26 10:14:47 -0600

    Ok
    Many

---
+++ js/questions.js
@@ -0,0 +1,25 @@
+(function(){
+    const tittleQuestions = [...document.querySelectorAll('.questions__title')];
+    console.log(tittleQuestions)
+
+
+    tittleQuestions.forEach(question =>{
+      question.addEventListener('click', ()=>{
+       let height = 0;
+       let answer = question.nextElementSibling;
+       let addPadding = question.parentElement.parentElement;
+
+       addPadding.classList.toggle('questions__padding--add');
+       
+       question.children[0].classList.toggle('questions__arrow--rotate');
+
+
+       if(answer.clientHeight === 0){
+        height = answer.scrollHeight;
+       }
+
+       answer.style.height = `${height}px`;
+    });
+
+    });
+})();
